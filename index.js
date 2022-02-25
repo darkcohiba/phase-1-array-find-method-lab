2abaf59fcf6d83c3ff73af4d0c4280b041bfb579
Sam <samuel.waters@gmail.com>
2022-02-25 16:03:58 -0700

    commit tree efadfeeffb74b33f51bc412e32d84f17d794e3d5

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+// const superbowlWin = obj => obj.find(win)
+// console.log(superbowlWin())
+
+// function win(obj){
+//     return obj.result === 'W'
+// }
+
+
+// function superbowlWin(obj){
+//     let win = record.find(obj => obj.result === 'W')
+//     console.log(win.year)
+//     return(win.year)
+// }
+
+function superbowlWin(obj){
+    let win = obj.find((record) => record.result === 'W')
+    if (win){
+        return win.year
+    }else{
+        return undefined
+    }
+}
